aa14ba47be80032d70397937d3383693eb470b0d
Peter S <peter@anyline.io>
2018-12-03 21:34:04 +0100

    a bit of sound :)

--- game.js
+++ game.js
@@ -43,10 +43,10 @@ preload ()
 	game.load.image('start_screen', 'gfx/startscreen.png');
 	game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
 	game.load.audio('music', 'sfx/theme.ogg');
-	game.load.audio('shredSound', 'sfx/placeholder.ogg');
-	game.load.audio('popSound', 'sfx/placeholder.ogg');
+	game.load.audio('shredSound', 'sfx/splatter.ogg');
+	game.load.audio('popSound', 'sfx/balloon_pop.ogg');
 	game.load.audio('explosionSound', 'sfx/placeholder.ogg');
-	game.load.audio('birdSound', 'sfx/placeholder.ogg');
+	game.load.audio('birdSound', 'sfx/placeholder.ogg');	
 }
 
 create ()
